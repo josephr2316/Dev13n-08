48473d8a6f34601edc97b2f722564721ed61804c
José Miguel Rodríguez Kelly <josephr2316@gmail.com>
2026-01-25 01:02:36 -0400

    Fix issue with the function ById and parsing

--- Okane.Application/FileExpensesRepository.cs
+++ Okane.Application/FileExpensesRepository.cs
@@ -23,24 +23,25 @@ public class FileExpensesRepository : IRepository<Expense>
     {
         if (!File.Exists(_filePath))
             return null;
-
+        
         foreach (var line in File.ReadLines(_filePath)){
             var parts = line.Split(',');
-            if (parts.Length > 0 && parts[0] == id.ToString()){
+            if (parts.Length > 0 && int.Parse(parts[0]) == id) {
                 return new Expense { Id = int.Parse(parts[0]), Amount = int.Parse(parts[1]), CategoryName = parts[2] };
             }
         }
 
-        return !File.Exists(_filePath) ? null : File.ReadLines(_filePath).
-            Select(line =>
-                {
-                    var parts = line.Split(',');
-                    return new Expense
-                    {
-                        Id = int.Parse(parts[0]), Amount = int.Parse(parts[1]), CategoryName = parts[2]
-                    };
-                }
-            ).FirstOrDefault(e => e.Id == id);
+        return null;
+        // return !File.Exists(_filePath) ? null : File.ReadLines(_filePath).
+        //     Select(line =>
+        //         {
+        //             var parts = line.Split(',');
+        //             return new Expense
+        //             {
+        //                 Id = int.Parse(parts[0]), Amount = int.Parse(parts[1]), CategoryName = parts[2]
+        //             };
+        //         }
+        //     ).FirstOrDefault(e => e.Id == id);
     }
 
     public IEnumerable<Expense> All()
