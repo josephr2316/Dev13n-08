9f2c54ecde635b7beca1c0b832a6cf146ba2ed23
José Miguel Rodríguez Kelly <josephr2316@gmail.com>
2026-01-30 00:42:55 -0400

    Refactor Expense clas

--- Okane.Application/Expense.cs
+++ Okane.Application/Expense.cs
@@ -4,6 +4,7 @@ public class Expense
 {
     public int Id { get; set; }
     public int Amount { get; set; }
-    public required string CategoryName { get; set; }
+    public int CategoryId { get; set; }
+    public Category? Category { get; set; }
     public string? Description { get; set; }
 }
